5562d168b95efdf41d8a73027d6900ad5f05b42a
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-07 13:22:32 +0200

    initial commit

---
+++ src/main/java/com/terracustosapi/terracustos/TerraCustosApplication.java
@@ -0,0 +1,13 @@
+package com.terracustosapi.terracustos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraCustosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraCustosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terracustosapi/terracustos/TerraCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terracustosapi.terracustos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
