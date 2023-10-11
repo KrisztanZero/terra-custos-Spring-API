57357049e00a7d6c87a6036c17d2c96b4eeff1d5
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-11 12:56:52 +0200

    add session

---
+++ src/main/java/com/terracustosapi/terracustos/Models/Session.java
@@ -0,0 +1,19 @@
+package com.terracustosapi.terracustos.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("sessions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Session {
+    @Id
+    private String sessionId;
+    private String userId;
+}
---
+++ src/main/java/com/terracustosapi/terracustos/Services/SessionService.java
@@ -0,0 +1,32 @@
+package com.terracustosapi.terracustos.Services;
+
+import com.terracustosapi.terracustos.IRepositories.ISessionRepository;
+import com.terracustosapi.terracustos.Models.Session;
+import com.terracustosapi.terracustos.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class SessionService implements ISessionService {
+
+    @Autowired
+    private ISessionRepository sessionRepository;
+    private String generateToken(){
+        return UUID.randomUUID().toString();
+    }
+    @Override
+    public Session generateSession(User user){
+        Session session = new Session(generateToken(), user.getId());
+
+        sessionRepository.save(session);
+
+        return session;
+    }
+
+    @Override
+    public Session getSession(String id){
+        return sessionRepository.findById(id).orElseThrow();
+    }
+}
