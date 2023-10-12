9257c31344b5302f3bfe2077858c4833bb6adab0
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-12 16:51:36 +0200

    add constructor for user data mapping

--- src/main/java/com/terracustosapi/terracustos/Dtos/UserDto.java
+++ src/main/java/com/terracustosapi/terracustos/Dtos/UserDto.java
@@ -1,5 +1,6 @@
 package com.terracustosapi.terracustos.Dtos;
 
+import com.terracustosapi.terracustos.Models.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +14,9 @@ public class UserDto {
     private String username;
     private String email;
     private String password;
+
+    public UserDto(User user) {
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
 }
