2c438d04abd4efe7bc6f35bbbeb14d3df7cb038f
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-12 16:46:02 +0200

    change api route in GreetingController

--- src/main/java/com/terracustosapi/terracustos/Controllers/GreetingController.java
+++ src/main/java/com/terracustosapi/terracustos/Controllers/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,10 +15,11 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/Greeting")
 public class GreetingController {
     @Autowired
     private IAuthService authService;
-    @GetMapping("/Greeting")
+    @GetMapping()
     public String welcome() {
         return "Welcome";
     }
