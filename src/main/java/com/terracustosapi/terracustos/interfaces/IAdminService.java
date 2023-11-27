c235071766af2f6d9209e9ea3bdf75570fc8dabd
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-11-27 20:20:35 +0100

    create admin service

---
+++ src/main/java/com/terracustosapi/terracustos/interfaces/IAdminService.java
@@ -0,0 +1,9 @@
+package com.terracustosapi.terracustos.interfaces;
+
+import com.terracustosapi.terracustos.models.User;
+
+import java.util.List;
+
+public interface IAdminService {
+    List<User> getALl();
+}
